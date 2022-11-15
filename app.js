d89acd18370acd2f73ce9ae2783f8fecdd8e4b9f
cha <chyb627@naver.com>
2022-11-15 22:15:01 +0900

    회원가입 + cors문제 해결

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const cors = require('cors');
+
 const postRouter = require('./routes/post');
+const userRouter = require('./routes/user');
 const db = require('./models');
 
 const app = express();
@@ -8,6 +11,14 @@ db.sequelize.sync()
         console.log('db 연결 성공');
     })
     .catch(console.error);
+
+app.use(cors({
+    // origin: 'http://localhost:3060'
+    origin: '*',
+    credentials: false,
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
 app.get('/', (req,res) => {
     res.send('hello express');
@@ -26,6 +37,7 @@ app.get('/posts', (req,res) => {
 });
 
 app.use('/post', postRouter);
+app.use('/user', userRouter);
 
 app.listen(3065, () => {
     console.log('서버 실행 중!');
